f6364d3e0edfb99cab8433d6f53df3a0766b00a1
cadetharoonmehsud-wq <cadetharoonmehsud@gmail.com>
2026-02-17 10:42:50 -0800

    my web project

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+arr=np.array([2,4,5])
+print(arr.ndim)
+print(arr.shape)
+print(arr.size)
+print(arr. dtype )
+print(np.zeros(5) )
+print(np.ones(5) )
+print(np.arange(1,10) )
+
+#import pandas as pd
+#data={
+    ##"name":["Ahmad","tufail","sara"],
+    #"Marks":[50,70,56]
+    
+#}
+#df=pd.DataFrame(data)
+#df["passed"]=df["Marks"]>70
+#print(df)
+#print(df["name"])
+#Average=df["Marks"].mean()
+#print("Average Marks:",Average)
